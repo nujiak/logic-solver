5bb0b0cd40f28050fbec5c739b8c2e02af4cf692
Chua Kai Jun <chuakaijun.ckj@gmail.com>
2024-04-18 11:47:51 +0800

    Fix equivalence equality operator

--- src/Propositions/BinaryOperations/Equivalence.cpp
+++ src/Propositions/BinaryOperations/Equivalence.cpp
@@ -7,10 +7,10 @@ std::string Equivalence::getString() const {
 
 bool Equivalence::operator==(const std::shared_ptr<WellFormedFormula> &other) const {
     if (auto otherEquivalence = std::dynamic_pointer_cast<Equivalence>(other)) {
-        return ((this->leftOperand == otherEquivalence->leftOperand) &&
-                (this->rightOperand == otherEquivalence->rightOperand)) ||
-               ((this->leftOperand == otherEquivalence->rightOperand) &&
-                (this->rightOperand == otherEquivalence->leftOperand));
+        return ((*this->leftOperand == otherEquivalence->leftOperand) &&
+                (*this->rightOperand == otherEquivalence->rightOperand)) ||
+               ((*this->leftOperand == otherEquivalence->rightOperand) &&
+                (*this->rightOperand == otherEquivalence->leftOperand));
     }
     return false;
 }
